aa9487543bbe1eb7647c3c6ecbb1149b73357285
z0374 <victormacedo2001@hotmail.com>
2025-12-22 15:59:11 -0300

    fix: added updating assets

--- templateCatalog01.js
+++ templateCatalog01.js
@@ -170,7 +170,7 @@ if (canListItems) {
             const currentData = dataRead('assets', {id: userState.select[0].replace(/[^0-9]/g, "")},env);
             await saveUserState(env, userId, userState);
             userState.select.push(currentData);
-            await sendMessage(`OK sr. ${userName}!\n Deseja alterar a ${userState.titulo.replace(/[^a-zA-Z]/g, "")}\n De ${currentData.data}\nPara ${messageText}`, chatId, env);
+            await sendMessage(`OK sr. ${userName}!\n Deseja alterar a ${userState.select[0].replace(/[^a-zA-Z]/g, "")}\n De ${currentData.data}\nPara ${messageText}`, chatId, env);
             await sendMessage("/SIM   |   /NAO", chatId, env);
             break;
             
